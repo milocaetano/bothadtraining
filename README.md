c006374115fd4b564753d5aaff882b37554f6298
milocaetano <camilo.s.caetano@gmail.com>
2023-02-16 20:32:02 -0300

    hello python

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # bothadtraining
+
+
+
+python -m venv mybothad
+pip install numpy
+pip freeze > requirements.txt
+pip install -r requirements.txt
---
+++ hello.py
@@ -0,0 +1,13 @@
+class MinhaClasse:
+    def meu_metodo(self):
+        print('Olá do meu método!')
+
+
+
+
+# Cria uma instância de MinhaClasse
+objeto = MinhaClasse()
+
+
+objeto.meu_metodo()
+
